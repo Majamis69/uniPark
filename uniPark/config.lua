827322bc4a562dce7fe8db1f5a6ce88b6d0ff72d
Majamis69 <145318941+Majamis69@users.noreply.github.com>
2024-04-29 16:24:32 +0300

    Delete uniPark directory

--- uniPark/config.lua
+++
@@ -1,5 +0,0 @@
-Config = {}
-
-Config.Price = 50
-Config.Jobs = {"police"}
-Config.Notification = 'ox_lib' -- Use 'ox_lib' if you want use ox lib notification. If you want use okokNotify then use 'okok'. If you want use your own type 'custom' and make export on client/functions.lua.
